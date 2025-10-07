e4b81089a481e1ef391388c15048f564161fae1b
Alex Skarapad <alexskarapad@MacBook-Air-Alex.local>
2025-10-07 10:29:22 +0300

    Update

--- src/lab02/typles.py
+++ src/lab02/typles.py
@@ -3,22 +3,19 @@ def format_record(rec: tuple[str, str, float]):
     if not isinstance(rec, tuple):
         raise TypeError(f"Ожидается tuple, получен {type(rec).__name__}")
     
-    
     if len(rec) != 3:
         raise ValueError(f"Ожидается кортеж из 3 элементов, получено {len(rec)}")
-    
+        
+    fio, group, gpa = rec
     
     if not isinstance(fio, str):
         raise TypeError(f"ФИО должен быть строкой, получен {type(fio).__name__}")
     
-    
     if not isinstance(group, str):
         raise TypeError(f"Группа должна быть строкой, получен {type(group).__name__}")
     
-    
     if not isinstance(gpa, (int, float)):
         raise TypeError(f"GPA должен быть числом, получен {type(gpa).__name__}")
-    
     
     if gpa < 0 or gpa > 5:
         raise ValueError(f"GPA должен быть в диапазоне от 0 до 5, получено {gpa}")
@@ -41,4 +38,3 @@ print(format_record(("Иванов Иван Иванович", "BIVT-25", 4.6)))
 print(format_record(("Петров Пётр", "IKBO-12", 5.0)))
 print(format_record(("Петров Пётр Петрович", "IKBO-12", 5.0)))
 print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
-
