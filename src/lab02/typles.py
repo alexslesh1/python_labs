08bb08d5119fa2c687a8523ea3e58dd67101a766
Alex Skarapad <alexskarapad@MacBook-Air-Alex.local>
2025-10-07 10:33:04 +0300

    Update

--- src/lab02/typles.py
+++ src/lab02/typles.py
@@ -39,4 +39,4 @@ print(format_record(("Петров Пётр", "IKBO-12", 5.0)))
 print(format_record(("Петров Пётр Петрович", "IKBO-12", 5.0)))
 print(format_record(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
 
-#1
+#12323
